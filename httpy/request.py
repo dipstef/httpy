3b0be66ae6255b9ce837db4d3ef7f13d2162ae45
dipstef <dipstef@gmail.com>
2014-06-09 19:13:36 +0100

    updated request

--- httpy/request.py
+++ httpy/request.py
@@ -3,10 +3,11 @@ from .headers import HttpHeaders
 
 class HttpRequest(object):
 
-    def __init__(self, url, method, headers=None):
+    def __init__(self, url, method, headers=None, data=None):
         self.url = url
         self.method = method.upper()
         self.headers = HttpHeaders(headers or {})
+        self.data = data
 
     def __repr__(self):
         return '%s: %s' % (self.method, self.url)
