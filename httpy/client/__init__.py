dcaa66d15b134caf954ac2ca8b3353aaccf62be2
dipstef <dipstef@gmail.com>
2014-08-10 13:40:53 +0100

    client interface

--- httpy/client/__init__.py
+++ httpy/client/__init__.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from .requests import user_agent, cookie_jar, HttpRequests, HttpyRequest
 
 
@@ -10,3 +11,16 @@ class HttpClient(HttpRequests):
         timeout = kwargs.pop('timeout', self._timeout_default)
 
         return super(HttpClient, self)._request(method, url, params, data, headers, timeout=timeout, **kwargs)
+
+    @abstractmethod
+    def execute(self, request, **kwargs):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self._close()
+
+    def _close(self):
+        pass
