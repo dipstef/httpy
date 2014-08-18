2ffec4fc79087141c89434e8991dc969250f46d6
dipstef <dipstef@gmail.com>
2014-08-18 18:35:55 +0100

    links downloads new version

--- httpy/error/error.py
+++ httpy/error/error.py
@@ -1,8 +1,11 @@
+from unicoder import byte_string
+
+
 class HttpError(Exception):
     def __init__(self, request, *args, **kwargs):
-        super(HttpError, self).__init__(str(request), *args, **kwargs)
+        super(HttpError, self).__init__(request, *args, **kwargs)
         self.request = request
-        self.message = '%s on: %s' % (self.__class__.__name__, str(request))
+        self.message = '%s on: %s' % (self.__class__.__name__, byte_string(request))
 
     def __str__(self):
         return self.message
